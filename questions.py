4928b2e07ae33e52b193ce27df4a261319c4c7f9
Ulises Rodriguez <ulises25103@gmail.com>
2025-03-16 11:32:52 -0300

    juego modificado, no accede a las 3 listas usando indices

--- questions.py
+++ questions.py
@@ -1,4 +1,5 @@
 import random
+import sys
 # Preguntas para el juego
 questions = [
 "¿Qué función se usa para obtener la longitud de una cadena en Python?",
@@ -23,32 +24,32 @@ answers = [
 # Índice de la respuesta correcta para cada pregunta, el el mismo orden que las preguntas
 correct_answers_index = [1, 2, 0, 3, 1]
 
+#crea una nueva lista con la pregunta, la respuesta y el indice de la respuesta correcta, haciendolo de forma random.
+questions_to_ask = random.choices(list(zip(questions, answers, correct_answers_index)), k=3)
+
 # El usuario deberá contestar 3 preguntas
-for _ in range(3):
+for pregunta, opciones, correcta in questions_to_ask:
 # Se selecciona una pregunta aleatoria
-    question_index = random.randint(0, len(questions) - 1)
+    print(pregunta)
 # Se muestra la pregunta y las respuestas posibles
-print(questions[question_index])
 
-for i, answer in enumerate(answers[question_index]):
-    print(f"{i + 1}. {answer}")
+    for i, answer in enumerate(opciones, start = 1):
+        print(f"{i}. {answer}")
 
-# El usuario tiene 2 intentos para responder correctamente
-for intento in range(2):
-    user_answer = int(input("Respuesta: "))
-    if user_answer < 1 or user_answer > 4:
-        print("Respuesta no válida")
-        exit()
-    
-# Se verifica si la respuesta es correcta
-    if user_answer == correct_answers_index[question_index]:
-        print("¡Correcto!")
-        break
-    else:
-# Si el usuario no responde correctamente después de 2 intentos,
-# se muestra la respuesta correcta
-        print("Incorrecto. La respuesta correcta es:")
-        print(answers[question_index]
-        [correct_answers_index[question_index]])
-# Se imprime un blanco al final de la pregunta
-print()
+    # El usuario tiene 2 intentos para responder correctamente
+    for intento in range(2):
+        user_answer = int(input("Respuesta: "))
+        if user_answer < 1 or user_answer > 4:
+            print("Respuesta no válida")
+            exit()
+        
+    # Se verifica si la respuesta es correcta
+        if user_answer -1 == correcta:
+            print("¡Correcto!")
+            break
+        else:
+    # Si el usuario no responde correctamente después de 2 intentos,
+    # se muestra la respuesta correcta
+            print("Incorrecto. La respuesta correcta es:")
+    # Se imprime un blanco al final de la pregunta
+    print()
